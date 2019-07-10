58648d179c7838830f53b6f35d93fe93cfe1cacb
asemqaffaf <asem@qaffaf.com>
2019-07-10 11:52:13 +0300

    july qize

---
+++ expression.js
@@ -0,0 +1,12 @@
+/*
+ * Programming Quiz: First Expression 
+ *
+ * Write an expression that uses at least three, different, arithmetic operators
+ * to log the number 42 to the console.
+ */
+
+// this expression equals 4, change it to equal 42
+
+console.log(7*7);
+console.log(42*1);
+console.log(84/2);
